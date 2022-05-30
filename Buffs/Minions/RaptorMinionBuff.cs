7a1d7b05c96470c72571c8087beabd4739575b8b
GeorgeFeldy <georgefeldy@gmail.com>
2022-05-30 15:31:11 +0300

    Base work on staff

---
+++ Buffs/Minions/RaptorMinionBuff.cs
@@ -0,0 +1,34 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+using NaturalVariety.Projectiles.Minions;
+
+namespace NaturalVariety.Buffs.Minions
+{
+    public class RaptorMinionBuff : ModBuff
+    {
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Raptor minion");
+			Description.SetDefault("A falcon will fight for you during daytime, an owl during nighttime");
+
+			Main.buffNoSave[Type] = true; // This buff won't save when you exit the world
+			Main.buffNoTimeDisplay[Type] = true; // The time remaining won't display on this buff
+		}
+
+		public override void Update(Player player, ref int buffIndex)
+		{
+			// If the minions exist reset the buff time, otherwise remove the buff from the player
+			if (player.ownedProjectileCounts[ModContent.ProjectileType<RaptorMinion>()] > 0)
+			{
+				player.buffTime[buffIndex] = 18000;
+			}
+			else
+			{
+				player.DelBuff(buffIndex);
+				buffIndex--;
+			}
+		}
+	}
+}
